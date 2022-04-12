aa658bbf72f4f89c3e614a634f4c4f45a720978b
Carl Johnson <me@carlmjohnson.net>
2022-04-12 10:00:02 -0400

    Docs: Add explanations

--- variables.go
+++ variables.go
@@ -4,8 +4,13 @@ package versioninfo
 import "time"
 
 var (
-	Version    = "unknown"
-	Revision   = "unknown"
+	// Version will be the version tag if the binary is built with "go install url/tool@version".
+	// If the binary is built some other way, it will be "devel".
+	Version = "unknown"
+	// Revision is taken from the vcs.revision tag in Go 1.18+.
+	Revision = "unknown"
+	// LastCommit is taken from the vcs.time tag in Go 1.18+.
 	LastCommit time.Time
+	// DirtyBuild is taken from the vcs.modified tag in Go 1.18+.
 	DirtyBuild = true
 )
