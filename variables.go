c3e61e76645a41b48855e486a5dd5bacdc498ca0
Carl Johnson <me@carlmjohnson.net>
2022-05-09 16:04:30 -0400

    Docs: Fix Version default

--- variables.go
+++ variables.go
@@ -5,7 +5,7 @@ import "time"
 
 var (
 	// Version will be the version tag if the binary is built with "go install url/tool@version".
-	// If the binary is built some other way, it will be "devel".
+	// If the binary is built some other way, it will be "(devel)".
 	Version = "unknown"
 	// Revision is taken from the vcs.revision tag in Go 1.18+.
 	Revision = "unknown"
