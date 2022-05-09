54d5ce0617ae3cb3156c1e8093dc00afb2f336ba
Carl Johnson <me@carlmjohnson.net>
2022-05-09 15:37:10 -0400

    Use testscript for testing (#2)

--- test/go.mod
+++ test/go.mod
@@ -2,6 +2,9 @@ module github.com/carlmjohnson/versioninfo/test
 
 go 1.18
 
-require github.com/carlmjohnson/versioninfo v0.0.0
+require github.com/rogpeppe/go-internal v1.8.1
 
-replace github.com/carlmjohnson/versioninfo => ../
+require (
+	github.com/pkg/diff v0.0.0-20210226163009-20ebb0f2a09e // indirect
+	gopkg.in/errgo.v2 v2.1.0 // indirect
+)
--- test/revision_test.go
+++ test/revision_test.go
@@ -1,16 +1,29 @@
 package main_test
 
 import (
+	"os"
+	"path/filepath"
+	"strings"
 	"testing"
 
-	"github.com/carlmjohnson/versioninfo"
+	"github.com/rogpeppe/go-internal/testscript"
 )
 
 func Test(t *testing.T) {
-	if versioninfo.Revision == "unknown" {
-		t.Fatal("bad versioninfo.Revision")
-	}
-	if versioninfo.LastCommit.IsZero() {
-		t.Fatal("bad versioninfo.LastCommit")
-	}
+	testscript.Run(t, testscript.Params{
+		Dir: "testdata",
+		Setup: func(e *testscript.Env) error {
+			gomod := `
+module main
+
+require github.com/carlmjohnson/versioninfo v0.0.0
+
+replace github.com/carlmjohnson/versioninfo => WORKDIR/..
+`
+			wd, _ := os.Getwd()
+			gomod = strings.Replace(gomod, "WORKDIR", wd, 1)
+			os.MkdirAll(filepath.Join(e.WorkDir, "main"), 0755)
+			return os.WriteFile(filepath.Join(e.WorkDir, "main", "go.mod"), []byte(gomod), 0644)
+		},
+	})
 }
