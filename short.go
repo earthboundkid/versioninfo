e83fa128fb71fa2634786a5fb960859d6387f8a8
Carl Johnson <me@carlmjohnson.net>
2022-06-21 11:05:37 -0400

    Short: Add rev- to commit info; remove redundant -dirty from version

--- short.go
+++ short.go
@@ -9,17 +9,18 @@ func Short() string {
 		parts = append(parts, Version)
 	}
 	if Revision != "unknown" && Revision != "" {
+		parts = append(parts, "rev")
 		commit := Revision
 		if len(commit) > 7 {
 			commit = commit[:7]
 		}
 		parts = append(parts, commit)
+		if DirtyBuild {
+			parts = append(parts, "dirty")
+		}
 	}
 	if len(parts) == 0 {
 		return "devel"
-	}
-	if DirtyBuild {
-		parts = append(parts, "dirty")
 	}
 	return strings.Join(parts, "-")
 }
